9b8b23b5dd1d05a4c22cd07de6df33a45401e996
Jang, Seung (Data61, Marsfield) <Seung.Jang@data61.csiro.au>
2025-09-02 10:14:33 +1000

    Test CWE-190

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s value\n", argv[0]);
+        return 1;
+    }
+    
+    int value = atoi(argv[1]);
+    int result = value + 1000;
+    printf("Result: %d\n", result);
     return 0;
 }
 
+/*
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+*/
